61d976ec70ca9fe354cf84db50e126f64b642b8f
Nootan Ghimire <nootan.ghimire@gmail.com>
2016-07-04 14:25:18 -0400

    Added Deploy code

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+error_exit ()
+{
+	echo "$1" 1>&2
+	exit 1
+}
+
+echo -n "Enter commit message":
+read commit_message
+cd /home/nootan/node_apps/first_timer/dev || error_exit "[x] Error Changing Directory. Exiting..."
+git add -A
+git commit -m "$commit_message"
+git push origin
+git push production master
