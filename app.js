71dfd65bfa738da3c7c25bc52c697a4f1025824d
Nootan Ghimire <nootan.ghimire@gmail.com>
2016-07-04 14:00:40 -0400

    Configed nginx to be a proxy

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+
 var express = require('express');
 var app = express();
+
+
 
 app.get('/', function (req, res) {
   res.send('Nootan Ghimire, Braggin: Hello World!');
 });
 
-app.listen(9000, function () {
-  console.log('Example app listening on port 9000!');
+app.enable('trust proxy');
+
+
+var port = process.env.NODE_ENV == 'development' ? 9000 : 9001;
+
+app.listen(port, function () {
+  console.log('Example app listening on port '+port+'!');
 });
 
